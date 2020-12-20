688a653c363c44525656a00ecd40e6f5711234cf
dmuraco3 <dylanjmuraco@gmail.com>
2020-12-20 16:01:48 -0500

    second fix for nav anchors having text-decoration

--- docs/navbar.js
+++ docs/navbar.js
@@ -1,33 +1,7 @@
 pages = ['home', 'projects', 'about', 'contact', 'blog']
 
-
-function addNavBar() { // this function adds the navbar to the page
-     
-    document.getElementById("nav").innerHTML += '<a class="blog" id="button blog" href="blog/index.html">BLOG</a>';;
-    document.getElementById("nav").innerHTML += '<a class="contact" id="button contact" href="contact/index.html">CONTACT</a>';
-    document.getElementById("nav").innerHTML += '<a class="about" id="button about" href="about/index.html">ABOUT</a>';
-    document.getElementById("nav").innerHTML += '<a class="projects" id="button projects" href="projects/index.html">PROJECTS</a>';
-    document.getElementById("nav").innerHTML += '<a class="home" id="button home" href="index.html">HOME</a>';
-    document.getElementById("nav").innerHTML += '<div class="logo"><a href="#">Dylan Muraco</a></div>';
-    
-    if(window.location.href == "http://127.0.0.1:5500/docs/index.html") { // for local development
-        document.getElementById("button home").id = "active";
-    }
-    if(window.location.href == "https://dmuraco3.github.io/") { // for public view
-        document.getElementById("button home").id = "active";
-    }
-    getUrl()
-}
-
-function getUrl() {
-    if((window.location.href).includes("blog")) {
-        document.getElementById("button blog").id = "active";
-    }
-}
-
-
 function test() {
-   if(window.location.href == "http://127.0.0.1:5500/docs/index.html" || window.location.href == "https://dmuraco3.github.io/") {
+   if(window.location.href == "http://127.0.0.1:5500/docs/index.html" || window.location.href == "https://dmuraco3.github.io/index.html") {
     document.getElementById("nav").innerHTML += '<a class="blog" id="button" href="blog/index.html">BLOG</a>';;
     document.getElementById("nav").innerHTML += '<a class="contact" id="button" href="contact/index.html">CONTACT</a>';
     document.getElementById("nav").innerHTML += '<a class="about" id="button" href="about/index.html">ABOUT</a>';
